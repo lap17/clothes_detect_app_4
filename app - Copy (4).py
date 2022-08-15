facf7b1f4e769b60ab909f1ec054b260d9ef70f1
lusha17 <antonlushkin2307@yandex.ru>
2022-08-15 12:07:48 +0300

    add match

--- app - Copy (4).py
+++ app - Copy (4).py
@@ -272,110 +272,8 @@ def func_web(model):
                     df = pd.DataFrame(result_queue)
                     labels_placeholder.write(df.to_html(escape=False), unsafe_allow_html=True)
     
+    
 def func_video(model):
-    def load_image_databunch(input_path, classes):
-        """
-        Code to define a databunch compatible with model
-        """
-        tfms = get_transforms(
-            do_flip=False,
-            flip_vert=False,
-            max_rotate=0,
-            max_lighting=0,
-            max_zoom=1,
-            max_warp=0,
-        )
-
-        data_bunch = ImageDataBunch.single_from_classes(
-            Path(input_path), classes, ds_tfms=tfms, size=640
-        )
-
-        return data_bunch
-    
-    def load_model(data_bunch, model_type, model_name):
-        """
-        Function to create and load pretrained weights of convolutional learner
-        """
-        learn = create_cnn(data_bunch, model_type, pretrained=False)
-        learn.load(model_name)
-        return learn
-
-
-    class SaveFeatures:
-        features = None
-
-        def __init__(self, m):
-            self.hook = m.register_forward_hook(self.hook_fn)
-            self.features = None
-
-        def hook_fn(self, module, input, output):
-            out = output.detach().cpu().numpy()
-            if isinstance(self.features, type(None)):
-                self.features = out
-            else:
-                self.features = np.row_stack((self.features, out))
-
-        def remove(self):
-            self.hook.remove()
-    
-    def image_to_vec(url_img, hook, learner):
-        _ = learner.predict(Image(pil2tensor(url_img, np.float32).div_(255)))
-        vect = hook.features[-1]
-        return vect
-
-    def get_vect(url_img, conv_learn, hook):
-        vect = image_to_vec(url_img, hook, conv_learn)
-        return vect
-    
-    def get_list_similar_images(url_img, conv_learn, hook, lsh, output_path, n_items):
-        ## Converting Image to vector
-        vect = image_to_vec(url_img, hook, conv_learn)
-        #print(vect)
-        ## Finding approximate nearest neighbours using LSH
-        response = lsh.query(vect, num_results=n_items+1, distance_func="hamming")
-        list_images = []
-        for res in response:
-            list_images.append(res[0][1])
-        return list_images
-    
-    def get_most_similar_image(img1, list_similar_images, w , h):
-        #img1 = cv.imread('cropped_image.jpg',cv.IMREAD_GRAYSCALE)
-        img1 = cv2.cvtColor(img1, cv.COLOR_RGB2GRAY)
-        img1 = resize(img1,w, h)
-        bool_find = False
-        for path_similar_image in list_similar_images:
-            img2 = cv.imread('cropped_dataset/'+path_similar_image,cv.IMREAD_GRAYSCALE) 
-            good_matches1, keypoints11, keypoints21 = get_key_points_by_images(img1, img2)
-            good_matches2, keypoints12, keypoints22 = get_key_points_by_images(img2, img1)
-            if (len(good_matches1) > 40) | (len(good_matches2) > 40):
-                bool_find = True
-                break
-        if bool_find:
-            img3 = cv2.cvtColor(img1, cv.COLOR_GRAY2RGB)
-        else:
-            img3 = None
-        return bool_find, img3
-    
-    
-    def detect_sku(im, cropped_image_cv):
-        w, h = im.size
-        if (w>540) | (h>540):
-            if w > h:
-                correction = w - 540
-            else:
-                correction = h - 540
-            im = im.resize((w-correction,h-correction), pil_img.ANTIALIAS)
-        elif (w<320) | (h<320):
-            if h > w:
-                correction = 456 - w
-            else:
-                correction = 456 - h
-            im = im.resize((w+correction,h+correction), pil_img.ANTIALIAS)
-        im = im.convert('RGB')
-        list_similar_images = get_list_similar_images(im, learner, sf, lsh, "output/output.png", 4)
-        bool_find, most_similar_image = get_most_similar_image(cropped_image_cv, list_similar_images, w , h)
-        return bool_find, most_similar_image
-            
     RTC_CONFIGURATION = RTCConfiguration(
        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
     ) 
@@ -383,55 +281,12 @@ def func_video(model):
     CLASSES_CUSTOM = [ 'short sleeve top', 'long sleeve top','short sleeve outwear','long sleeve outwear','vest','sling','shorts','trousers','skirt','short sleeve dress', 'long sleeve dress','vest dress','sling dress']
     
 
-    DEFAULT_CONFIDENCE_THRESHOLD = 0.5
+    DEFAULT_CONFIDENCE_THRESHOLD = 0.4
 
     result_queue = [] 
     global frames_count
     frames_count = 0
-    
-    classes = [ 'short sleeve top', 'long sleeve top','short sleeve outwear','long sleeve outwear','sling','shorts','trousers','skirt','short sleeve dress', 'long sleeve dress', 'vest dress','sling dress']
-    data_bunch = load_image_databunch("cropped_dataset", classes)
-    learner = load_model(data_bunch, models.resnet34, "stg1-rn34")
-    sf = SaveFeatures(learner.model[1][5])
-    lsh = pickle.load(open("lsh.p", "rb"))
-    
-    detector = cv.BRISK_create()
-    descriptor = cv.SIFT_create()
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
-    FLANN = cv.FlannBasedMatcher(indexParams = index_params, searchParams = search_params)
-    ratio_thresh_for_key_points = 0.8
-    
-    def get_key_points_by_images(image1, image2):
-        detector = cv.BRISK_create()
-        descriptor = cv.SIFT_create()
-        keypoints1, descriptors1 = features(image1, detector, descriptor)
-        keypoints2, descriptors2 = features(image2, detector, descriptor)
-        good_matches = matcher(image1, image2, keypoints1, keypoints2, descriptors1, descriptors2)
-        return good_matches, keypoints1, keypoints2
 
-    def matcher(image1, image2, keypoints1, keypoints2, descriptors1, descriptors2):
-        descriptors1 = np.float32(descriptors1)
-        descriptors2 = np.float32(descriptors2)
-        matches = FLANN.knnMatch(queryDescriptors = descriptors1, trainDescriptors = descriptors2, k = 2)
-        good_matches = []
-        for m, n in matches:
-                if m.distance < ratio_thresh_for_key_points * n.distance:
-                    good_matches.append(m)
-        return good_matches
-
-    def features(image, detector, descriptor):
-        keypoints = detector.detect(image, None)
-        keypoints, descriptors = descriptor.compute(image, keypoints)
-        return keypoints, descriptors
-    
-    
-    def img_to_html(img):
-        retval, buffer_img= cv2.imencode('.jpg', img)
-        data = base64.b64encode(buffer_img).decode("utf-8")
-        html = "<img src='data:image/jpg;base64," + data + f"""' style='display:block;margin-left:auto;margin-right:auto;width:200px;border:0;'>"""
-        return html
 
     def get_preds(img):
         return model([img]).xyxy[0].numpy()
@@ -452,7 +307,6 @@ def func_video(model):
                 color_dict[index] = (255,0,0)
         return color_dict
     
-    
     def create_player():
         return MediaPlayer(path)
     
@@ -462,7 +316,7 @@ def func_video(model):
             global frames_count 
             frames_count+=1
         #print(frames_count)
-        if frames_count%10==0:
+        if frames_count%5==0:
             pass
         else:
             return frame
@@ -470,7 +324,6 @@ def func_video(model):
         img_ch = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         result = get_preds(img_ch)
         result = result[np.isin(result[:,-1], target_class_ids)]  
-        dict_cropped_image = {}
         for bbox_data in result:
             xmin, ymin, xmax, ymax, conf, label = bbox_data
             if conf > confidence_threshold:
@@ -486,26 +339,18 @@ def func_video(model):
                 text_for_vis = '{} {}'.format(class_, str(conf.round(2)))
                 img = cv2.putText(img, text_for_vis, (int(xtext), int(ytext)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, rgb_colors[label], 2,)
                 if agree:
-                    w, h, t = cropped_image.shape
+                    time_detect = datetime.datetime.now(pytz.timezone("America/New_York")).replace(tzinfo=None).strftime("%m-%d-%y %H:%M:%S")
                     cropped_image = img[ymin:ymax, xmin:xmax]
-                    dict_cropped_image[w+h] = cropped_image
+                    retval, buffer_img= cv2.imencode('.jpg', cropped_image)
+                    data = base64.b64encode(buffer_img).decode("utf-8")
+                    html = "<img src='data:image/jpg;base64," + data + f"""' style='display:block;margin-left:auto;margin-right:auto;width:200px;border:0;'>"""
+                    with lock:
+                        result_queue.insert(0, {'object': class_, 'time_detect': time_detect, 'confident': str(conf.round(2)), 'img': html})
                 img = cv2.rectangle(img, p0, p1, rgb_colors[label], 2) 
                 ytext = ymin - 10 if ymin - 10 > 10 else ymin + 15
                 xtext = xmin + 10
                 text_for_vis = '{} {}'.format(class_, str(conf.round(2)))
                 img = cv2.putText(img, text_for_vis, (int(xtext), int(ytext)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, rgb_colors[label], 2,)
-            if agree & len(dict_cropped_image)!=0:
-                max_element = max(dict_cropped_image.keys())
-                cropped_image_cv = dict_cropped_image[max_element]
-                cropped_image_pil = pil_img.fromarray(cropped_image_cv)
-                bool_find, sku_image = detect_sku(cropped_image_pil, cropped_image_cv)
-                if bool_find:
-                    html_sku_image = img_to_html(sku_image)
-                    html_cropped_image_cv = img_to_html(cropped_image_cv)
-                else:
-                    html_cropped_image_cv = 'No match found'
-                with lock:
-                    result_queue.insert(0, {'cropped_frame': html_cropped_image_cv, 'sku': html_sku_image})
         return av.VideoFrame.from_ndarray(img, format="bgr24")
     
     
